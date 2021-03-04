d45e09bd9d7167dd819edb00645dba4403850e6d
Fernando <fernando.chaves16@hotmail.com>
2021-03-04 06:19:04 -0300

    error handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,22 +5,19 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user } = await authenticateUser.execute({
-      email,
-      password
-    });
+  const { user } = await authenticateUser.execute({
+    email,
+    password
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return res.json({ user });
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
-  }
+  return res.json({ user });
+
 });
 
 export default sessionsRouter;
