44ab3165272b4550b7651d2fa2fab81e7be48096
Fernando <fernando.chaves16@hotmail.com>
2021-03-04 03:32:31 -0300

    uploading avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -31,7 +32,21 @@ usersRouter.post('/', async (req, res) => {
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async(req, res) => {
 
-  return res.json({ ok: true });
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+
+  } catch(err) {
+    return res.status(400).json({ error: err.message });
+  }
 });
 
 export default usersRouter;
